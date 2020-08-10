de39e03db242947d4acfd97686ea2a5705e7a1ec
David Kirkby <dkirkby@uci.edu>
2020-08-10 12:28:29 -0700

    Start simple css

--- docs/arxiver.js
+++ docs/arxiver.js
@@ -8,47 +8,53 @@ function text(content) {
     return $("<span></span>").html(content);
 }
 
-$(document).ready(function() {
-    $("#submit").click(function (e) {
-        let id = $("#id").val();
-        console.log('id >>' + id + '<<');
-        if(!id) {
-            alert("Enter a valid arxiv identifier");
-            $("#id").focus();
-            return false;
-        }
-        $.ajax({
-            dataType: "jsonp",
-            url: url + '?id=' + id + '&root=' + $("#root").val(),
-            success: function(result) {
-                let div = $("<div class='result'></div>");
-                if(result["status"] == "ok") {
-                    div.append(link(result["pdfLink"], link["pdfLink"]));
-                    div.append(text(" &rarr; "));
-                    div.append(link(result["category"], result["categoryLink"]));
-                    div.append(text(" / "));
-                    div.append(link(result["year"], result["yearLink"]));
-                    div.append(text(" / "));
-                    div.append(link(result["name"], result["fileLink"]));
-                    let size = new Number(result["pdfSize"]);
-                    if(size == 0) {
-                        div.append(text(" (already downloaded)"));
-                    }
-                    else {
-                        div.append(text(" (" + (size / (1 << 20)).toFixed(2) + "Mb)"));
-                    }
+function submit(e) {
+    let id = $("#id").val();
+    console.log('id >>' + id + '<<');
+    if(!id) {
+        alert("Enter a valid arxiv identifier");
+        $("#id").focus();
+        return false;
+    }
+    $.ajax({
+        dataType: "jsonp",
+        url: url + '?id=' + id + '&root=' + $("#root").val(),
+        success: function(result) {
+            let div = $("<div class='result'></div>");
+            if(result["status"] == "ok") {
+                div.append(link(result["pdfLink"], result["pdfLink"]));
+                div.append(text(" &rarr; "));
+                div.append(link(result["category"], result["categoryLink"]));
+                div.append(text(" / "));
+                div.append(link(result["year"], result["yearLink"]));
+                div.append(text(" / "));
+                div.append(link(result["name"], result["fileLink"]));
+                let size = new Number(result["pdfSize"]);
+                if(size == 0) {
+                    div.append(text(" (already downloaded)").addClass("note"));
                 }
                 else {
-                    div.append(text(result["message"]).addClass("error"));
+                    div.append(text(" (" + (size / (1 << 20)).toFixed(2) + "Mb)"));
                 }
-                $("#results").append(div);
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                let div = $("<div class='result'></div>");
-                div.append(text(errorThrown).addClass("error"));
-                $("#results").append(div);
             }
-        });
-        return false;
+            else {
+                div.append(text(result["message"]).addClass("error"));
+            }
+            $("#results").append(div);
+            $("#id").val("").focus();
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            let div = $("<div class='result'></div>");
+            div.append(text(result["id"] + " &rarr; "));
+            div.append(text(errorThrown).addClass("error"));
+            $("#results").append(div);
+            $("#id").val("").focus();
+        }
     });
+    return false;
+}
+
+$(document).ready(function() {
+    $("#submit").click(submit);
+    $("#id").focus();
 });
